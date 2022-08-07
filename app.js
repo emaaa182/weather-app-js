e4df7211eaef0e6b5996d04aba275d78131e52ba
emanuel acosta <wherehog21@gmail.com>
2022-08-07 20:42:55 -0300

    inicial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+window.addEventListener('load', ()=> {
+    let lon
+    let lat
+
+    let temperaturaValor= document.getElementById('temperatura-valor')
+    let temperaturaDescripcion= document.getElementById('temperatura-descripcion')
+
+    let ubicacion = document.getElementById('ubicacion')
+    let iconoAnimado = document.getElementById('icono-animado')
+
+    let vientoVelocidad= document.getElementById('viento-velocidad')
+
+
+    if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition( posicion => {
+           //console.log(posicion.coords.latitude)
+           lon = posicion.coords.longitude
+           lat = posicion.coords.latitude
+            //ubicación actual    
+           //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=67988b5cd4f06c57c61e24a67618c907`
+           const url = `https://api.openweathermap.org/data/2.5/weather?q=La Plata&lang=es&units=metric&appid=67988b5cd4f06c57c61e24a67618c907`
+         //  console.log(url)
+           fetch(url)
+           .then(response=>{return response.json()})
+           .then(data=>{
+               let temp = Math.round(data.main.temp)
+               temperaturaValor.textContent= `${temp} °c`
+               let desc = data.weather[0]. description
+               temperaturaDescripcion.textContent = desc.toUpperCase()
+               ubicacion.textContent = data.name
+               vientoVelocidad.textContent =`${data.wind.speed}m/s`
+               switch (data.weather[0].main){
+                   case'Clear':
+                   iconoAnimado.src ='animated/day.svg'
+                   break;
+                   case'Cloud':
+                   iconoAnimado.src ='animated/cloudy-day-1.svg'
+                   break;
+                   case 'Thunderstorm':
+                    iconoAnimado.src='animated/thunder.svg'
+                    break;
+                    case 'Drizzle':
+                    iconoAnimado.src='animated/rainy-2.svg'
+                    break;
+                    case 'Rain':
+                    iconoAnimado.src='animated/rainy-7.svg'
+                    break;
+                    case 'Snow':
+                    iconoAnimado.src='animated/snowy-6.svg'
+                    break;
+                    case 'Atmosphere':
+                    iconoAnimado.src='animated/weather.svg'
+                    break;
+                    default:
+                    iconoAnimado.src='animated/cloudy-day-1.svg'
+
+
+               }
+                
+           })
+           .catch(error=>{
+               console.log(error)
+               
+           })
+
+         
+            })
+
+          
+    }
+})
